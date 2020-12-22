9c5f74d1ba46c88fc9281ac27f5facdc64db47b3
Kimhawoon <lemon_741@naver.com>
2020-12-22 22:43:43 +0900

    termProject

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT ? process.env.PORT : 8080;
+const PORT = process.env.PORT;
 const morgan = require('morgan');
 const path = require('path');
 const nunjucks = require('nunjucks');
